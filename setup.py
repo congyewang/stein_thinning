41e5a900d51592cd8f3acee881deedb43ce43c4c
Congye Wang <w.congye2@newcastle.ac.uk>
2023-04-23 19:16:05 +0100

    Add centralised KGM kernel

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='stein_thinning',
-    version='1.2.2',
+    version='1.3.0',
     description='Optimally compress sampling algorithm outputs',
     url='https://github.com/wilson-ye-chen/stein_thinning',
     author='Stein Thinning team',
